3f6651442d0b5d5fe8b4acd92ba491fb1b01a10d
zhangqi <zhangqi@t8.io>
2015-11-25 16:15:16 +0800

    make system message view model ui ok

--- T8ChatUIKit/T8SystemMessageViewModel.m
+++ T8ChatUIKit/T8SystemMessageViewModel.m
@@ -7,10 +7,11 @@
 //
 
 #import "T8SystemMessageViewModel.h"
+#import "T8TextViewModel.h"
 
 @interface T8SystemMessageViewModel ()
 {
-    
+    T8TextViewModel *_contentLabel;
 }
 
 @end
@@ -22,7 +23,9 @@
     self = [super initWithMessage:message];
     if (self) {
         
-        
+        _contentLabel = [[T8TextViewModel alloc] initWithText:@"user#4889 joined chat" font:[UIFont systemFontOfSize:12]];
+        _contentLabel.textColor = [UIColor whiteColor];
+        [self addSubmodel:_contentLabel];
         
     }
     return self;
@@ -30,15 +33,21 @@
 
 - (void)layoutForContainerSize:(CGSize)containerSize
 {
-    self.frame = CGRectMake(0, 0, containerSize.width, 40);
+    [_contentLabel layoutForContainerSize:CGSizeMake(containerSize.width - 120, 0)];
+    _contentLabel.frame = CGRectMake((containerSize.width - _contentLabel.frame.size.width)/2, 5, _contentLabel.frame.size.width + 10, _contentLabel.frame.size.height + 10);
+    
+    self.frame = CGRectMake(0, 0, containerSize.width, _contentLabel.frame.size.height + 10);
 }
 
 - (void)bindViewToContainer:(UIView *)container
 {
     [super bindViewToContainer:container];
     
-    UIView *view = self.boundView;
-    view.backgroundColor = [UIColor greenColor];
+    UILabel *contentLabel = (UILabel *)_contentLabel.boundView;
+    contentLabel.backgroundColor = [UIColor colorWithRed:206/255.0 green:213/255.0 blue:217/255.0 alpha:1];
+    contentLabel.layer.cornerRadius = 5;
+    contentLabel.layer.masksToBounds = YES;
+    contentLabel.textAlignment = NSTextAlignmentCenter;
 }
 
 @end
